a56c1d0f207446dd4bd2b2e23f4489958c15fe76
unknown <dima.sb713@gmail.com>
2021-08-01 15:47:53 +0300

    Dijkstra algorithm added

--- graph.cpp
+++ graph.cpp
@@ -3,23 +3,26 @@
 using namespace std;
 
 void Adjacency_matrix();                            // adjacency matrix (breadth-first graph search)
-void Own_adjacency_matrix();
+void Own_adjacency_matrix();                        // your own adjacency matrix (breadth-first graph search)
+void Dijkstra_algorithm();                          // realization of the shortest way
 
 int main()
 {
     int a; 
 
     cout << "\n\t--------------------------------------\n\t|      Action ways with graphs       |\n\t--------------------------------------\n\t|\t\t\t\t     |";
-    cout<<"\n\t|  Adjacency matrix (prepared) - 1   |\n\t|  Adjacency matrix (your own) - 2   |";
+    cout<<"\n\t|  Adjacency matrix (prepared) - 1   |\n\t|  Adjacency matrix (your own) - 2   |\n\t|  Dijkstra algorithm - 3\t     |";
     cout<<"\n\t|\t\t\t\t     |\n\t--------------------------------------\n\n\t  Enter type of action : ";
     cin >> a;
-    cout << "\n\t___________________________";
+    cout << "\n_____________________________________________________";
     
     switch (a) 
     {
         case 1:Adjacency_matrix();
             break;
         case 2:Own_adjacency_matrix();
+            break;
+        case 3:Dijkstra_algorithm();
             break;
         default:
             break;
@@ -62,8 +65,12 @@ void Adjacency_matrix()
     for (int i = 0; i < 7; i++)
         nodes[i] = 0;                                   // for start all vertices = 0
 
-    Queue.push(0);                                      // 1-st vertice goes in a queue
-    cout << "\n\n\t   Start vertice -> " << Queue.front() + 1 << "\n\n    ";
+    int b;
+    cout << "\n\n\t   Start vertice -> ";
+    cin >> b;
+    cout << "\n\n    ";
+
+    Queue.push(b - 1);                                  // 1-st vertice goes in a queue
 
     while (!Queue.empty())
     {                                                   // while queue isn`t empty
@@ -130,13 +137,17 @@ void Own_adjacency_matrix()         // algorithm the same as in the function: Ad
         printf("\n");
     }
 
-    int nodes[7];                                       // graph vertices
+    int nodes[100];                            // graph vertices
     for (int i = 0; i < 7; i++)
         nodes[i] = 0;                                   // for start all vertices = 0
     queue<int> Queue;
 
-    Queue.push(0);                                      // 1-st vertice goes in a queue
-    cout << "\n\n\t   Start vertice -> " << Queue.front() + 1 << "\n\n    ";
+    int b;
+    cout << "\n\n\t   Start vertice -> ";
+    cin >> b;
+    cout << "\n\n    ";
+
+    Queue.push(b-1);                                      // 1-st vertice goes in a queue
 
     while (!Queue.empty())
     {                                                   // while queue isn`t empty
@@ -166,4 +177,121 @@ void Own_adjacency_matrix()         // algorithm the same as in the function: Ad
         delete[] a[i];                  //clear memory
                                         //
     delete[] a;                         //
+}
+
+
+void Dijkstra_algorithm()
+{
+    int n, oriented, temp;
+    cout << "\n\n\t  Enter count of elements : ";
+    cin >> n;
+    cout << "\n\t  Is this graph oriented? ( 1-yes, 0-no ) : ";
+    cin >> oriented;
+    cout << "\n";
+
+    int** a, count = 1, ver = 1, k;
+
+    a = new int* [n];               //
+    for (int i = 0; i < n; i++)     //dyn. memory for matrix
+        a[i] = new int[n];          //
+
+    int* dist = new int[n];         // min distance
+    int* visit = new int[n];        // visited vetices
+
+    if(oriented!=1)
+    {
+        cout << "   <->    Enter the distance between : \n\n";
+        for (int i = 0; i < n; i++)
+        {
+            a[i][i] = 0;
+            for (int j = i + 1; j < n; j++) 
+            {
+                cout << "\t\t" << i + 1 << " --> " << j + 1 << "  = ";
+                cin >> temp;
+                a[i][j] = temp;
+                a[j][i] = temp;
+            }
+        }
+    }
+
+    else
+    {
+        cout << "   <->    Enter the distance between : \n\n";
+        for (int i = 0; i < n; i++)
+        {
+            a[i][i] = 0;
+            for (int j = 0; j < n; j++) 
+            {
+                if(i!=j)
+                {
+                    cout <<"\t\t" << i + 1 << " --> " << j + 1 << "  = ";
+                    cin >> temp;
+                    a[i][j] = temp;
+                }
+            }
+        }
+    }
+    cout << "\n\t\t   Matrix\n\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "\t";
+        for (int j = 0; j < n; j++)
+            printf("%-5d ", a[i][j]);
+        cout<<"\n";
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        dist[i] = 1000;
+        visit[i] = 1;
+    }
+    cout << "\n______________________________________________________\n\n\t  Enter the start : ";
+    cin >> k;
+    dist[k-1] = 0;
+
+    int limit;
+    int min;
+    do {
+        limit = 1000;
+        min = 1000;
+        for (int i = 0; i < n; i++)             // if the vertice isn`t visited and dist < min (10000)
+        { 
+            if ((visit[i] == 1) && (dist[i] < min))
+            {                                           //reassign our values
+                min = dist[i];
+                limit = i;
+            }
+        }
+        
+        if (limit != 1000)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (a[limit][i] > 0)
+                {
+                    temp = min + a[limit][i];
+                    if (temp < dist[i])
+                    {
+                        dist[i] = temp;
+                    }
+                }
+            }
+            visit[limit] = 0;
+        }
+    } while (limit < 1000);
+
+    cout<<"\nThe shortest ways to another vertices: \n";
+    for (int i = 0; i < n; i++)
+    {
+        if (k != i + 1)
+            cout << "\t\t" << k << " --> " << i + 1 << "  = " << dist[i] << endl;
+    }
+
+    for (int i = 0; i < n; i++)         //
+        delete[] a[i];                  //clear memory
+                                        //
+    delete[] a;                         //
+    delete[] dist;
+    delete[] visit;
 }
