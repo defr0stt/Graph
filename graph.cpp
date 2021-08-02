803f963274d82beb9cb881c47180a9654b440d9f
unknown <dima.sb713@gmail.com>
2021-08-02 11:43:29 +0300

    Prima algorithm added

--- graph.cpp
+++ graph.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void Adjacency_matrix();                            // adjacency matrix (breadth-first graph search)
 void Own_adjacency_matrix();                        // your own adjacency matrix (breadth-first graph search)
 void Dijkstra_algorithm();                          // realization of the shortest way
+void Prima_algorithm();                             // the minimum skeletal tree of the graph
 
 int main()
 {
@@ -12,6 +13,7 @@ int main()
 
     cout << "\n\t--------------------------------------\n\t|      Action ways with graphs       |\n\t--------------------------------------\n\t|\t\t\t\t     |";
     cout<<"\n\t|  Adjacency matrix (prepared) - 1   |\n\t|  Adjacency matrix (your own) - 2   |\n\t|  Dijkstra algorithm - 3\t     |";
+    cout << "\n\t|  Prima algorithm - 4\t\t     | ";
     cout<<"\n\t|\t\t\t\t     |\n\t--------------------------------------\n\n\t  Enter type of action : ";
     cin >> a;
     cout << "\n_____________________________________________________";
@@ -23,6 +25,8 @@ int main()
         case 2:Own_adjacency_matrix();
             break;
         case 3:Dijkstra_algorithm();
+            break;
+        case 4:Prima_algorithm();
             break;
         default:
             break;
@@ -104,7 +108,7 @@ void Own_adjacency_matrix()         // algorithm the same as in the function: Ad
     cout << "\n\n\t  Enter count of elements : ";
     cin >> n;
 
-    int **a, count=1, ver=1,k;
+    int **a, count=1, ver=1;
 
     a = new int*[n];                //
     for (int i = 0; i < n; i++)     //dyn. memory for matrix
@@ -294,4 +298,71 @@ void Dijkstra_algorithm()
     delete[] a;                         //
     delete[] dist;
     delete[] visit;
+}
+
+
+void Prima_algorithm() 
+{
+        int a, b, u, v, n, i, j, ne = 1;
+        int visited[10] = { 0 }, min, mincost = 0, cost[100][100];
+        int path[100] = { 0 }; 
+        int path_index = 0;
+
+        cout << "\n\n\tEnter the count of vertices :  "; cin >> n;
+        cout << "\tEnter the matrix : \n";
+
+
+        for (int i = 1; i <=n; i++)
+        {
+            cost[i][i] = 999;
+            for (int j = i + 1; j <=n; j++)
+            {
+                cout << "\t\t" << i << " --> " << j << "  = ";
+                cin >> cost[i][j];
+                if (cost[i][j] == 0)
+                {
+                    cost[i][j] = 999;
+                    cost[j][i] = 999;
+                }
+                else
+                    cost[j][i] = cost[i][j];
+            }
+        }
+        visited[1] = 1;
+        cout << "\n";
+
+        while (ne < n)
+        {
+            for (i = 1, min = 999; i <=n; i++)
+                for (j = 1; j <=n; j++)
+                    if (cost[i][j] < min)
+                        if (visited[i] != 0)
+                        {
+                            min = cost[i][j];
+                            a = u = i;
+                            b = v = j;
+                        }
+            if (visited[u] == 0 || visited[v] == 0)
+            {
+                path[path_index] = b;
+                path_index++;
+                ne++; 
+                mincost += min;
+                visited[b] = 1;
+
+            }
+            cost[a][b] = cost[b][a] = 999;
+        }
+
+
+        cout << "\n";
+
+        cout <<"  " << 1 << " --> ";
+        for (int i = 0; i < n - 1; i++)
+        {
+            cout << path[i];
+            if (i < n - 2) cout << " --> ";
+        }
+
+        cout << "\n  Min cost = " << mincost << endl;
 }
